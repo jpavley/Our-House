c75cf6441037fb79053653e8585122796a7d199b
John Pavley <johnpavley-imac5k@Johns-iMac-3.local>
2017-08-04 21:10:57 -0400

    removed IBInspectibles cause it corrupted storyboard. Complete grid

--- Our House/DesignableHouseView.swift
+++ Our House/DesignableHouseView.swift
@@ -13,15 +13,19 @@ import UIKit
 /// and maintains a perfect square aspect ratio.
 @IBDesignable class DesignableHouseView: UIView {
     
-    @IBInspectable var fillColor: UIColor = UIColor.lightGray
-    @IBInspectable var lineColor: UIColor = UIColor.black
-    @IBInspectable var lineThickness: CGFloat = 1.0
-    @IBInspectable var rows: Int = 26
-    @IBInspectable var cols: Int = 15
-    @IBInspectable var dashSegmentWidth: CGFloat = 2.0
-    @IBInspectable var dashGapWidth: CGFloat = 2.0
+    let fillColor = UIColor.lightGray
+    let lineColor = UIColor.black
     
-    var cellSideLength: CGFloat = 0.0
+    let lineThickness: CGFloat = 1.0
+    
+    let rows: Int = 26
+    let cols: Int = 15
+    
+    let dashSegmentWidth: CGFloat = 2.0
+    let dashGapWidth: CGFloat = 4.0
+    
+    var gridHOffset: CGFloat = 0.0
+    var gridVOffset: CGFloat = 0.0
     
     override func draw(_ rect: CGRect) {
         drawBackground()
@@ -37,30 +41,49 @@ import UIKit
     func drawGrid() {
         print(bounds)
         
-        calcCellSideLength()
+        let cellSideLength = calcCellSideLength()
+        gridHOffset = calcGridHOffset(cellSideLength: cellSideLength)
+        gridVOffset = calcGridVOffset(cellSideLength: cellSideLength)
         
         for col in 0..<cols {
             for row in 0..<rows {
-                drawCell(col: col, row: row)
+                drawCell(col: col, row: row, gridHOffset: gridHOffset)
             }
         }
     }
     
-    func calcCellSideLength() {
+    func calcCellSideLength() -> CGFloat {
         let cellWidthCandidate = bounds.width/CGFloat(cols)
         let cellHeightCandidate = bounds.height/CGFloat(rows)
-        
-        cellSideLength = min(cellWidthCandidate, cellHeightCandidate)
+        return min(cellWidthCandidate, cellHeightCandidate)
     }
     
-    func drawCell(col: Int, row: Int) {
-        let x1 = cellSideLength * CGFloat(col)
+    func calcCellBounds(col: Int, row: Int, cellSideLength: CGFloat) -> CGRect {
+        let x1 = (cellSideLength * CGFloat(col)) + gridHOffset
         let y1 = cellSideLength * CGFloat(row)
+        return CGRect(x: x1, y: y1, width: cellSideLength, height: cellSideLength)
+    }
+    
+    func calcGridHOffset(cellSideLength: CGFloat) -> CGFloat {
+        let gridWidth = cellSideLength * CGFloat(cols)
+        let offset = (bounds.width - gridWidth) / 2
+        return offset
+    }
+    
+    func calcGridVOffset(cellSideLength: CGFloat) -> CGFloat {
+        let gridHight = cellSideLength * CGFloat(rows)
+        let offset = (bounds.height - gridHight) / 2
+        return offset
+    }
+
+    
+    func drawCell(col: Int, row: Int, gridHOffset: CGFloat) {
+        let cellSideLength = calcCellSideLength()
+        let cellBounds = calcCellBounds(col: col, row: row, cellSideLength: cellSideLength)
         
-        let cellPath = UIBezierPath(rect: CGRect(x: x1, y: y1, width: cellSideLength, height: cellSideLength))
+        let cellPath = UIBezierPath(rect: cellBounds)
         cellPath.lineWidth = lineThickness
         
-        cellPath.move(to: CGPoint(x: x1, y: x1))
         let pattern: [CGFloat] = [dashSegmentWidth, dashGapWidth]
         cellPath.setLineDash(pattern, count: 2, phase: 0.0)
         lineColor.setStroke()
