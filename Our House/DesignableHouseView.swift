d44e0876b6c26a27cb65023c2950a6cc8941be46
John Pavley <jpavley@gmail.com>
2017-09-09 18:51:12 -0400

    gird change

--- Our House/DesignableHouseView.swift
+++ Our House/DesignableHouseView.swift
@@ -8,94 +8,108 @@
 
 import UIKit
 
-/// Grid branch:
-/// Create a 15 x 26 grid of cells that fits any iOS device screen size
-/// and maintains a perfect square aspect ratio.
+/// Grass Sun Sky branch:
+/// A test of our coorindate system with three elements.
 @IBDesignable class DesignableHouseView: UIView {
     
-    let fillColor = UIColor.lightGray
-    let lineColor = UIColor.black
-    let backColor = UIColor.black
+    struct Grass {
+        let bounds = CGRect(x: 1, y: 76, width: 58, height: 27)
+        let color = UIColor.green
+    }
     
-    let lineThickness: CGFloat = 1.0
+    struct Sky {
+        let bounds = CGRect(x: 1, y: 1, width: 58, height: 75)
+        let color = UIColor.blue
+    }
     
-    let rows: Int = 26
-    let cols: Int = 15
+    struct Sun {
+        let bounds = CGRect(x: 40, y: 7, width: 13, height: 14)
+        let color = UIColor.yellow
+    }
     
-    let dashSegmentWidth: CGFloat = 2.0
-    let dashGapWidth: CGFloat = 4.0
+    struct World {
+        let bounds: CGRect?
+        let rows: Int?
+        let cols: Int?
+        var cellSideLength: CGFloat = 0.0
+        var gridHOffset: CGFloat = 0.0
+        var gridVOffset: CGFloat = 0.0
+        
+        func calcCellSideLength() -> CGFloat? {
+            if let bounds = bounds, let rows = rows, let cols = cols {
+                let cellWidthCandidate = bounds.width/CGFloat(cols)
+                let cellHeightCandidate = bounds.height/CGFloat(rows)
+                return min(cellWidthCandidate, cellHeightCandidate)
+            }
+            return nil
+        }
+        
+        func calcCellBounds(col: Int, row: Int, cellSideLength: CGFloat) -> CGRect {
+            let x1 = (cellSideLength * CGFloat(col)) + gridHOffset
+            let y1 = cellSideLength * CGFloat(row)
+            return CGRect(x: x1, y: y1, width: cellSideLength, height: cellSideLength)
+        }
+        
+        func calcGridHOffset(cellSideLength: CGFloat) -> CGFloat? {
+            if let bounds = bounds, let cols = cols {
+                let gridWidth = cellSideLength * CGFloat(cols)
+                let offset = (bounds.width - gridWidth) / 2
+                return offset
+            }
+            return nil
+        }
+        
+        func calcGridVOffset(cellSideLength: CGFloat) -> CGFloat? {
+            if let bounds = bounds, let rows = rows {
+                let gridHight = cellSideLength * CGFloat(rows)
+                let offset = (bounds.height - gridHight) / 2
+                return offset
+            }
+            return nil
+        }
+        
+        init(bounds: CGRect, rows: Int, cols: Int) {
+            self.bounds = bounds
+            self.rows = rows
+            self.cols = cols
+            cellSideLength = calcCellSideLength()!
+            gridHOffset = calcGridHOffset(cellSideLength: cellSideLength)!
+            gridVOffset = calcGridVOffset(cellSideLength: cellSideLength)!
+        }
+        
+    }
     
-    var gridHOffset: CGFloat = 0.0
-    var gridVOffset: CGFloat = 0.0
-    
+    var world: World?
+        
     override func layoutSubviews() {
         super.layoutSubviews()
         self.setNeedsDisplay()
     }
     
     override func draw(_ rect: CGRect) {
+        
+        if world == nil {
+            world = World(bounds: bounds, rows: 104, cols: 60)
+        }
+        
         drawBackground()
-        drawGrid()
+        drawGrass()
+        //drawSky()
+        //drawSun()
     }
     
     func drawBackground() {
         let path = UIBezierPath(rect: bounds)
-        backColor.setFill()
+        UIColor.black.setFill()
         path.fill()
     }
     
-    func drawGrid() {
-        print(bounds)
-        
-        let cellSideLength = calcCellSideLength()
-        gridHOffset = calcGridHOffset(cellSideLength: cellSideLength)
-        gridVOffset = calcGridVOffset(cellSideLength: cellSideLength)
-        
-        for col in 0..<cols {
-            for row in 0..<rows {
-                drawCell(col: col, row: row, gridHOffset: gridHOffset)
-            }
-        }
-    }
-    
-    func calcCellSideLength() -> CGFloat {
-        let cellWidthCandidate = bounds.width/CGFloat(cols)
-        let cellHeightCandidate = bounds.height/CGFloat(rows)
-        return min(cellWidthCandidate, cellHeightCandidate)
-    }
-    
-    func calcCellBounds(col: Int, row: Int, cellSideLength: CGFloat) -> CGRect {
-        let x1 = (cellSideLength * CGFloat(col)) + gridHOffset
-        let y1 = cellSideLength * CGFloat(row)
-        return CGRect(x: x1, y: y1, width: cellSideLength, height: cellSideLength)
-    }
-    
-    func calcGridHOffset(cellSideLength: CGFloat) -> CGFloat {
-        let gridWidth = cellSideLength * CGFloat(cols)
-        let offset = (bounds.width - gridWidth) / 2
-        return offset
-    }
-    
-    func calcGridVOffset(cellSideLength: CGFloat) -> CGFloat {
-        let gridHight = cellSideLength * CGFloat(rows)
-        let offset = (bounds.height - gridHight) / 2
-        return offset
+    func drawGrass() {
+        let grass = Grass()
+        let path = UIBezierPath(rect: grass.bounds)
+        grass.color.setFill()
+        path.fill()
     }
 
     
-    func drawCell(col: Int, row: Int, gridHOffset: CGFloat) {
-        let cellSideLength = calcCellSideLength()
-        let cellBounds = calcCellBounds(col: col, row: row, cellSideLength: cellSideLength)
-        
-        let cellPath = UIBezierPath(rect: cellBounds)
-        cellPath.lineWidth = lineThickness
-        
-        let pattern: [CGFloat] = [dashSegmentWidth, dashGapWidth]
-        cellPath.setLineDash(pattern, count: 2, phase: 0.0)
-        fillColor.setFill()
-        cellPath.fill()
-        lineColor.setStroke()
-        cellPath.stroke()
-        
-    }
 }
